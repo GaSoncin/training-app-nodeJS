f366ae065a8bbd98552b1e7ee751106ccac22200
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 21:54:16 -0300

    API

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.get('/api/exemplo', (req, res) => {
+  res.json({mensagem:'back-end!'});
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta: ${PORT}`);
+});
